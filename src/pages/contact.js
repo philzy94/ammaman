ff96183cbf24f460120e07a2a706719138340a88
Philemon Shekari <holyphilzy@gmail.com>
2024-06-12 19:59:26 +0100

    update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -34,7 +34,7 @@ export default function Contact() {
             <svg className="mr-3 h-5 w-5 text-gray-600" fill="currentColor" viewBox="0 0 20 20">
               <path d="M18 2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h16zm-4.37 9.1L20 16v-2l-5.12-3.9L20 6V4l-10 8L0 4v2l5.12 4.1L0 14v2l6.37-4.9L10 14l3.63-2.9z" />
             </svg>
-            <span className="font-medium text-gray-700">holla@veekthoven.com</span>
+            <span className="font-medium text-gray-700">holyphilzy@gmail.com</span>
           </div>
           {/* <p className="mb-4 text-gray-600">
             <span className="font-bold uppercase">PS:</span> I am only open to freelance engagements
